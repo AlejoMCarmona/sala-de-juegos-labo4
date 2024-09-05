9dd6234136b93886c087ba01c08052744a6ad5fb
AlejoMCarmona <alejocarmona99@hotmail.com>
2024-09-05 18:05:08 -0300

    Primer sprint hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        { path: '', redirectTo: '/home', pathMatch: "full" },
+        { path: 'home', component: HomeComponent },
+        { path: 'quien-soy', component: QuienSoyComponent },
+        { path: 'login', component: LoginComponent },
+        { path: '**', component: PaginaNoEncontradaComponent },     
+];
