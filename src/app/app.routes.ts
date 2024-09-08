a32c20386cb88e74455eb0652c1ffd19a19ad429
AlejoMCarmona <alejocarmona99@hotmail.com>
2024-09-08 17:11:06 -0300

    Se hizo responsive y se agregó el componente de Registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './componentes/home/home.component';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 export const routes: Routes = [
         { path: '', redirectTo: '/home', pathMatch: "full" },
         { path: 'home', component: HomeComponent },
         { path: 'quien-soy', component: QuienSoyComponent },
         { path: 'login', component: LoginComponent },
+        { path: 'registro', component: RegistroComponent },
         { path: '**', component: PaginaNoEncontradaComponent },     
 ];
