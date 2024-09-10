5efb8b054981b4c19db4e94713d0237a077ada5a
AlejoMCarmona <alejocarmona99@hotmail.com>
2024-09-09 21:47:50 -0300

    Se agregó el favicon, las alertas personalizadas y cierta funcionalidad en Login y Registro

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ RouterLink, FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  public email!: string;
+  public password!: string;
+  public inicioExitoso!: boolean;
+
+  constructor(private router: Router) {}
+
+  public autocompletar() {
+    this.email = 'admin@test.com';
+    this.password = '12345';
+  }
+
+  public iniciarSesion() {
+    if (this.email === 'admin@test.com' && this.password === '12345') {
+      this.router.navigate(['juegos']);
+    } else {
+      this.lanzarError('Inicio de sesión fallido', 'Las credenciales ingresadas son incorrectas');
+    }
+  }
+
+  private lanzarError(titulo: string, mensaje: string) {
+    Swal.fire({
+      icon: "error",
+      title: titulo,
+      text: mensaje
+    });
+  }
 }
--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  public nombre: string = '';
+  public apellido: string = '';
+  public email: string = '';
+  public password: string = '';
+  public fechaNacimiento: string = '';
 
+  constructor(private router: Router) {}
+
+  public registrarUsuario() {
+    if (this.validarCampos()) {
+      this.router.navigate(['login']);
+    }
+  }
+
+  private validarCampos(): boolean {
+    if (!this.nombre || !this.apellido || !this.email || !this.password || !this.fechaNacimiento) {
+      this.lanzarError('Campos incompletos', 'Por favor, completa todos los campos.');
+      return false;
+    }
+
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(this.email)) {
+      this.lanzarError('Email inválido', 'Por favor, ingresa un correo electrónico válido.');
+      return false;
+    }
+
+    return true;
+  }
+
+  private lanzarError(titulo: string, mensaje: string) {
+    Swal.fire({
+      icon: "error",
+      title: titulo,
+      text: mensaje
+    });
+  }
 }
