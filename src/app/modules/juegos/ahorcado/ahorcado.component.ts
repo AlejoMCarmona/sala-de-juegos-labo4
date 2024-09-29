dfbb9fd553f672ca05b35df653c6f524850d8410
AlejoMCarmona <alejocarmona99@hotmail.com>
2024-09-28 23:20:54 -0300

    Ahorcado terminado

--- src/app/modules/juegos/ahorcado/ahorcado.component.ts
+++ src/app/modules/juegos/ahorcado/ahorcado.component.ts
@@ -12,10 +12,13 @@ export class AhorcadoComponent implements OnInit {
   private palabras: string[] = ['ELEFANTE', 'CONEJO', 'JIRAFA', 'LEON', 'SERPIENTE', 'CANGREJO', 'TIBURON', 'ARDILLA', 'GORILA', 'HORMIGA', 'AGUILA', 'CULEBRA', 'DROMEDARIO', 'FOCA', 'DELFIN', 'PINGUINO', 'PAVO', 'TORO', 'CABALLO', 'BURRO'];
   private palabraElegida: string = '';
   public palabraOculta: string = '';
-  public letras: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('');
+  public letras: string[] = 'QWERTYUIOPASDFGHJKLÑZXCVBNM'.split('');
+  public letrasDeshabilitadas: Set<string> = new Set();
   public intentosRestantes!: number;
   public puntuacion!: number;
   public estaLogueado: boolean = false;
+  private rutaUrlImagenes: string = "assets/ahorcado/";
+  public urlImagenActual: string = "";
 
   constructor(private auth: AuthService, private _mensajeService: MensajesService) {
     this.iniciarJuego();
@@ -31,13 +34,15 @@ export class AhorcadoComponent implements OnInit {
     this.palabraElegida = this.palabras[Math.floor(Math.random() * this.palabras.length)];
     this.palabraOculta = '_'.repeat(this.palabraElegida.length).split('').join(' ');
     this.intentosRestantes = 7;
+    this.urlImagenActual = this.rutaUrlImagenes + "ahorcado-" + this.intentosRestantes + ".png";
     this.puntuacion = 0;
+    this.letrasDeshabilitadas.clear();
   }
 
   public adivinarLetra(letra: string) {
     let nuevaPalabraOculta = this.palabraOculta.split(' ');
     let coincidencia = false;
-
+    
     for (let i = 0; i < this.palabraElegida.length; i++) {
       if (this.palabraElegida[i] === letra) {
         nuevaPalabraOculta[i] = letra;
@@ -46,6 +51,9 @@ export class AhorcadoComponent implements OnInit {
     }
 
     this.palabraOculta = nuevaPalabraOculta.join(' ');
+    console.log("Deshabilitando...");
+    this.deshabilitarLetra(letra);
+    console.log("Resultado has: " + this.letrasDeshabilitadas.has(letra));
 
     if (coincidencia) {
       this.puntuacion += 10; // +10 puntos por letra acertada
@@ -56,10 +64,15 @@ export class AhorcadoComponent implements OnInit {
     } else {
       this.intentosRestantes--;
       this.puntuacion -= 5; // Penalización de -5 puntos por intento fallido
+      this.urlImagenActual = this.rutaUrlImagenes + "ahorcado-" + this.intentosRestantes + ".png";
       if (this.intentosRestantes == 0) {
         this._mensajeService.lanzarMensajeError("GAME OVER", "La palabra era '" + this.palabraElegida + "'");
         this.iniciarJuego();
       }
     }
+  }
+
+  public deshabilitarLetra(letra: string) {
+    this.letrasDeshabilitadas.add(letra);
   }
 }
